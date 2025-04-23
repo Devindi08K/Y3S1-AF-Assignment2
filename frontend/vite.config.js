0b8d0d06ea1fba7c28ee3072b4a8e24979b7278f
Devindi08K <Devindi08@outlook.com>
2025-04-23 23:17:30 +0530

    fixed the deployment errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './', // Changed from '/' to './'
   resolve: {
     alias: {
       '@': '/src',
       'components': '/components'
     }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false
   }
 })
